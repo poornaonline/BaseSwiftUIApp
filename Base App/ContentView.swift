2c58700af45b58f673d97cfef8f36b1f17fd55e7
Poorna Jayasinghe <poornasjayasinghe@gmail.com>
2020-08-29 15:30:31 +1000

    Initial Commit

---
+++ Base App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Base App
+//
+//  Created by Poorna Jayasinghe on 2020-08-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Base AppTests/Base_AppTests.swift
@@ -0,0 +1,33 @@
+//
+//  Base_AppTests.swift
+//  Base AppTests
+//
+//  Created by Poorna Jayasinghe on 2020-08-29.
+//
+
+import XCTest
+@testable import Base_App
+
+class Base_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
