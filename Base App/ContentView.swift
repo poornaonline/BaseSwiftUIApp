e9cca1498ecebe84846725fd6e2974be587217ce
Poorna Jayasinghe <poornasjayasinghe@gmail.com>
2020-08-29 17:28:30 +1000

    Base UI

--- Base App/ContentView.swift
+++ Base App/ContentView.swift
@@ -9,8 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ForEach((1...4), id: \.self) { index in
+                PoornaCardRow()
+            }
+        }
+    }
+}
+
+
+struct PoornaCardRow: View {
+    var body: some View {
+        HStack {
+            ForEach((1...4), id: \.self) { index in
+                PoornaCard(title: "Poorna")
+            }
+        }.padding()
+    }
+}
+
+struct PoornaCard: View {
+
+    var title: String?
+
+    var isFaceUp = true
+
+    private let CORNER_RADIUS = CGFloat(10)
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                
+                RoundedRectangle(cornerRadius: CORNER_RADIUS)
+                    .foregroundColor(.white)
+                    .shadow(radius: CORNER_RADIUS)
+                
+                RoundedRectangle(cornerRadius: CORNER_RADIUS)
+                    .stroke(Color.red, lineWidth: 1)
+                    .foregroundColor(.red)
+                
+                Text(title!)
+                    .foregroundColor(Color.yellow)
+                    .fontWeight(.bold)
+                
+            } else {
+                RoundedRectangle(cornerRadius: CORNER_RADIUS)
+                    .foregroundColor(.red)
+                    .shadow(radius: CORNER_RADIUS)
+                
+            }
+        }
     }
 }
 
